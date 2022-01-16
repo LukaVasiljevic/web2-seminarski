25d9343527716b9aa3b9b4f4be4b2b39ff023d12
Luka Vasiljević <lukav217@gmail.com>
2022-01-16 12:34:08 +0100

    final changes

--- fe/frontend/src/app/app.module.ts
+++ fe/frontend/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +18,6 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     PageNotFoundComponent,
     HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
